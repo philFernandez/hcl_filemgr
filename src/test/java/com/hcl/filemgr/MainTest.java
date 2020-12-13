b327c32feca5eaa23754b27fc7628f1532fe492b
Phil Fernandez <norcal618@gmail.com>
2020-12-12 18:39:42 -0800

    unittest

---
+++ src/test/java/com/hcl/filemgr/MainTest.java
@@ -0,0 +1,14 @@
+package com.hcl.filemgr;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+    @Test
+    void isWrittable() {
+        assertTrue(true);
+    }
+}

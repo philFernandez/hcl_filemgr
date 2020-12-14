20f01ca6336fd0baaddb15db6b2a96559ffc8b31
Phil Fernandez <norcal618@gmail.com>
2020-12-14 01:16:59 -0800

    better clearScreen

--- src/main/java/com/hcl/tools/Utils.java
+++ src/main/java/com/hcl/tools/Utils.java
@@ -3,8 +3,9 @@ package com.hcl.tools;
 public class Utils {
 
     public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        // System.out.print("\033[H\033[2J");
+        // System.out.flush();
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
     }
 
     public static <T> T last(T[] array) {
